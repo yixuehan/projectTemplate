c649345aca4209fa8f7707684c820e45f7738c42
yixuehan <505703394@qq.com>
2018-11-10 20:37:16 +0800

    修改初始化脚本

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
+#设置mak、shell路径`
 shellpath=$PWD/$(dirname $0)
+mkhome=$shellpath/makeTemplate
+
 cd $shellpath
 mkdir ${PRONAME}/bin ${PRONAME}/log ${PRONAME}/lib ${PRONAME}/include ${PRONAME}/src ${PRONAME}/etc
+git submodule add https://github.com/yixuehan/makeTemplate.git
+git submodule add https://github.com/boostorg/boost.git
 git submodule update --init --recursive
 
 # 编译boost
 cd $shellpath/boost
-git submodule update --recursive
 ./bootstrap.sh --libdir=${PRONAME}/lib --includedir=${PRONAME}/include
 ./bjam cxxflags="-std=c++1z" variant=release install
 
-#设置mak路径
-mkhome=$shellpath/makeTemplate
+#拷贝配置文件
+cp -i ${mkhome}/etc/env.sh ${PRONAME}/etc/env.sh
+rm -ri ${PRONAME}/mak
+cp -R ${mkhome} ${PRONAME}/mak
+
+#提示
 oldpath=$mkhome
 cd $mkhome
 mkhome=$PWD
 cd $oldpath
-echo 增加[export MKHOME=$mkhome]
-echo 加载[. ${mkhome}/etc/env.sh]
+echo 在.bashrc中增加:
+echo export MKHOME=${PRONAME}/mak
+echo . ${PRONAME}/etc/env.sh
