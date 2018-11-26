16385a234fc18ffbdef2e3881f8cd8cf3ecac416
yixuehan <505703394@qq.com>
2018-11-26 16:01:52 +0800

    changes

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -8,12 +8,11 @@ fi
 
 #设置mak、shell路径`
 shellpath=$PWD/$(dirname $0)
-mkhome=$shellpath/makeTemplate
 
 cd ${PRONAME}
 mkdir ${PRONAME}/bin ${PRONAME}/log ${PRONAME}/lib ${PRONAME}/include ${PRONAME}/src ${PRONAME}/etc
 cd $shellpath
-#git submodule add https://github.com/boostorg/boost.git
+git submodule add https://github.com/boostorg/boost.git
 git submodule update --init --recursive
 
 # 编译boost
@@ -22,10 +21,6 @@ cd $shellpath/boost
 ./bjam cxxflags="-std=c++1z" variant=release install
 
 #提示
-oldpath=$mkhome
-cd $mkhome
-mkhome=$PWD
-cd $oldpath
 echo 在.bashrc中增加:
 echo export MKHOME='${PRONAME}/makeTemplate/mak'
 echo '. ${PRONAME}/projectTemplate/etc/env.sh'
