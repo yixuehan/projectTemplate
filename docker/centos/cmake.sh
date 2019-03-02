701d5c646af2f71dcdeb7d247344aa01e7148475
yixuehan <505703394@qq.com>
2019-03-02 15:48:50 +0800

    cmake

--- docker/centos/cmake.sh
+++ docker/centos/cmake.sh
@@ -3,6 +3,11 @@ version=3.13.1
 if [ ! -f cmake-${version}.tar.gz ]
 then
     wget https://github.com/Kitware/CMake/releases/download/v${version}/cmake-${version}.tar.gz
+    if [ ! $? -eq 0 ]
+    then
+        rm -rf cmake-${version}.tar.gz
+        exit 1
+    fi
     tar -xf cmake-${version}.tar.gz
 fi
 cd cmake-${version}
