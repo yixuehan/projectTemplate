ee373222c88428e384f5d62910a6309b8a77b9d4
xieqw <xieqw@deepblueai.com>
2020-04-29 09:26:20 +0800

    optimize

--- sh/env.sh
+++ sh/env.sh
@@ -59,15 +59,6 @@ export PYTHONPATH=$PYTHONPATH:${HOME}/usr/lib/python3.6/site-packages
 
 export LOGICAL_NUM=`cat /proc/cpuinfo | grep "processor" | wc -l`
 export PHYSICAL_NUM=`cat /proc/cpuinfo | grep "physical id" | uniq | wc -l`
-# 有些物理核心与逻辑核心数一样，编译时会崩溃，可能是内存不够，这种情况线程数 /2
-if [ ${PHYSICAL_NUM} -eq ${LOGICAL_NUM} ]
-then
-    PHYSICAL_NUM=$(expr ${PHYSICAL_NUM} / 2)
-    if [ ${PHYSICAL_NUM} -lt 1 ]
-    then
-        PHYSICAL_NUM=1
-    fi
-fi
 
 export PKG_CONFIG_PATH=${HOME}/usr/lib/pkgconfig
 
@@ -174,6 +165,7 @@ function build()
     then
         NUM=${MemAvailable}
     fi
+	echo "make -j${NUM} VERBOSE=1"
     make -j${NUM} VERBOSE=1
     cd ${old}
 }
