74d9915dd55aa73be2f119bd82586df4579d0d8a
yixuehan <505703394@qq.com>
2020-05-08 20:01:18 +0800

    save

--- mq/rabbitmq.sh
+++ mq/rabbitmq.sh
@@ -10,11 +10,13 @@ docker rm ${name}
 docker run -d \
            --network=host \
            --name ${name} \
+           -v /data/${image}/${tag}/data:/var/lib/rabbitmq \
            -e RABBITMQ_DEFAULT_VHOST=my_vhost \
            -e RABBITMQ_DEFAULT_USER=admin \
            -e RABBITMQ_DEFAULT_PASS=admin \
            ${image}:${tag}
 
+           # -v /data/${image}/${tag}/etc:/etc/rabbitmq \
            # -v /data/${image}/${tag}/data:/var/lib/rabbitmq \
            # --hostname myRabbit \
            # -p 5672:5672 \
