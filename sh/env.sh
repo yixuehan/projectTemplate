0c51d163b99d9d2201e7d4d3e9026c2b4585fadb
desert <505703394@qq.com>
2020-05-25 22:36:23 +0800

    save

--- sh/env.sh
+++ sh/env.sh
@@ -202,19 +202,19 @@ function re_link_gcc()
 ulimit -c unlimited
 
 # devtoolset-7
-if [ "$MKOSTYPE" = "centos" ]
-then
-    grep "release 8" /etc/centos-release &>/dev/null
-    if [ ! $? -eq 0 ]
-    then
-        #export PATH=/opt/rh/devtoolset-7/root/bin:$PATH
-        #export PATH=/opt/rh/rh-python36/root/bin:$PATH
-        source /opt/rh/devtoolset-8/enable
-        # source /opt/rh/rh-python36/enable
-        # source /opt/rh/rh-mysql80/enable
-        # scl enable devtoolset-7 bash
-    fi
-fi
+# if [ "$MKOSTYPE = "centos" ]
+# then
+#     grep "release 8" /etc/centos-release &>/dev/null
+#     if [ ! $? -eq 0 ]
+#     then
+#         #export PATH=/opt/rh/devtoolset-7/root/bin:$PATH
+#         #export PATH=/opt/rh/rh-python36/root/bin:$PATH
+#         source /opt/rh/devtoolset-8/enable
+#         # source /opt/rh/rh-python36/enable
+#         source /opt/rh/rh-mysql80/enable
+#         # scl enable devtoolset-7 bash
+#     fi
+# fi
 
 #if [ 4 -eq $SHLVL ] && [ $MKOSTYPE = "centos" ]
 #then
